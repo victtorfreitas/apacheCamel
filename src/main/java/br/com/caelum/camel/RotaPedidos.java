5d3bae8002426bb0f8844d0f273c0de314dd74c9
Victtor Freitas <victtorfreitas95@gmail.com>
2021-10-27 16:02:14 -0300

    Implement call http4

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,7 +1,9 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -13,15 +15,20 @@ public class RotaPedidos {
       @Override
       public void configure() {
         from("file:pedidos?delay=5s&noop=true")
-            .split()
-            .xpath("/pedido/itens/item")
-            .filter()
-            .xpath("/item/formato[text()='EBOOK']")
+            .setProperty("pedidoId", xpath("/pedido/id/text()"))
+            .setProperty("clientId", xpath("/pedido/pagamento/email-titular/text()"))
+            .split().xpath("/pedido/itens/item")
+            .filter().xpath("/item/formato[text()='EBOOK']")
             .log("${id}")
+            .setProperty("ebookId", xpath("/item/livro/codigo/text()"))
             .marshal().xmljson()
-            .log("${body}")
-            .setHeader("CamelFileName", simple("${file:name.noext}.json"))
-            .to("file:saida");
+            .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET))
+            .setHeader(Exchange.HTTP_QUERY,
+                simple(
+                    "ebookId=${property.ebookId}&"
+                        + "pedidoId=${property.pedidoId}&"
+                        + "clienteId=${property.clientId}"))
+            .to("http4://localhost:8080/webservices/ebook/item");
       }
     });
     context.start();
